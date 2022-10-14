bfe963a1c3a95e757eba51bede9b9cc098b46164
coop711 <coop711@icloud.com>
2022-10-15 01:58:49 +0900

    modified

--- R/mosaic_gg.R
+++ R/mosaic_gg.R
@@ -1,61 +1,67 @@
 mosaic_gg <-
-function(tbl, base_family = "", 
-                      ggtitle = "", 
-                      xlab = "", 
-                      ylab = "", 
-                      fill.name = ""){
-tbl.df <- as.data.frame(tbl)
-tbl.sum <- tapply(tbl.df$Freq, tbl.df[, 2], sum)
-tbl.p.m <- prop.table(tbl.sum)
-tbl.p <- prop.table(tbl)
-tbl.p.2 <- prop.table(tbl, margin = 2)
-tbl.p.df <- as.data.frame(tbl.p)
-tbl.p.df$width <- tbl.p.m[match(tbl.p.df[, 2], names(tbl.p.m))]
-tbl.p.df$height <- as.data.frame(tbl.p.2)$Freq
-tbl.p.df$label.height <- unlist(tapply(tbl.p.df$height, tbl.p.df[, 2], cumsum))
-x.center <- (cumsum(tbl.p.m) + c(0, head(cumsum(tbl.p.m), -1)))/2
-tbl.p.df$center <- x.center[match(tbl.p.df[, 2], names(x.center))]
-m1 <- ggplot(tbl.p.df, aes(x = center, y = height, width = width)) + 
-  geom_bar(aes(fill = vote), 
-           stat = "identity", 
-           col = "white", 
-           size = 1, 
-           position = position_stack(reverse = TRUE)) 
-m2 <- m1 + 
-  theme_bw(base_family = base_family)
-m3 <- m2 + 
-  geom_text(aes(x = center, y = 1.05), 
-            label = tbl.p.df[, 2], 
-            family = base_family)
-m4 <- m3 + 
-  geom_text(aes(x = center, y = label.height/2), 
-            label = format(tbl.df$Freq, big.mark = ","), 
-            position = position_stack(reverse = TRUE))
-x.breaks <- c(0, ifelse(cumsum(tbl.p.m) < 0.1, 0.0, cumsum(tbl.p.m)))
-x.label <- format(x.breaks * 100, 
-                  digits = 3, 
-                  nsmall = 1)
-y.breaks <- tbl.p.df$label.height
-# delta <- (max(y.breaks) - min(y.breaks)) / 20
-# y.breaks.sort <- sort(y.breaks)
-# diff(y.breaks.sort) < delta 
-# index <- which(diff(y.breaks.sort)  > delta)
-# y.breaks <- c(0, y.breaks.sort[c(index, length(y.breaks.sort))])
-y.label <- format(y.breaks * 100,
-                  digits = 2,
-                  nsmall = 1)
-m5 <- m4 + 
-  scale_x_continuous(name = xlab, 
-                     breaks = x.breaks, 
-                     label = x.label) + 
-  scale_y_continuous(name = ylab,
-                     breaks = y.breaks,
-                     label = y.label) + 
-  scale_fill_manual(name = fill.name, 
-                    values = rainbow(2)[2:1], 
-                    labels = tbl.df$vote, 
-                    guide = guide_legend()) +
-  ggtitle(ggtitle) +
-  theme(plot.margin = unit(c(1, 2, 1, 1), "lines"))
-return(m5)
+function(tbl){
+  tbl_df <- tbl %>%
+  as.data.frame
+  N <- length(levels(tbl_df[, 1]))
+#> data for mosaic coordinates
+  tbl_p_df <- tbl %>%
+    prop.table %>%
+    as.data.frame
+  tbl_p_m <- tbl_df %>%
+    `[`(, 3) %>%
+    tapply(tbl_df[, 2], sum) %>%
+    prop.table
+  tbl_p_df$width <- tbl_p_m[match(tbl_p_df[, 2], names(tbl_p_m))]
+  tbl_p_df$height <- tbl %>%
+    prop.table(margin = 2) %>%
+    as.data.frame %>%
+    `[`(, 3)
+  tbl_p_df$label_height <- unlist(tapply(tbl_p_df$height, 
+                                         tbl_p_df[, 2], 
+                                         function(x) cumsum(x) - x / 2))
+  tbl_p_df$y_breaks <- unlist(tapply(tbl_p_df$height, 
+                                     tbl_p_df[, 2], 
+                                     cumsum))
+  x_center <- cumsum(tbl_p_m) - tbl_p_m / 2
+  tbl_p_df$center <- x_center[match(tbl_p_df[, 2], names(x_center))]
+#> 
+#> breaks and labels
+  x_breaks <- c(0, ifelse(cumsum(tbl_p_m) < 0.1, 0.0, cumsum(tbl_p_m)))
+  x_label <- format(x_breaks * 100, 
+                    digits = 3, 
+                    nsmall = 1)
+  y_breaks <- tbl_p_df$y_breaks
+########
+  delta <- (max(y_breaks) - min(y_breaks)) / 20
+  y_breaks_sort <- sort(y_breaks)
+  diff(y_breaks_sort) < delta 
+  index <- which(diff(y_breaks_sort)  > delta)
+  y_breaks <- c(0, y_breaks_sort[c(index, length(y_breaks_sort))])
+  y_label <- format(y_breaks * 100,
+                    digits = 2,
+                    nsmall = 1)
+#######
+#> 
+  ggplot(tbl_p_df, 
+         aes(x = center, 
+             y = height, 
+             width = width)) + 
+    geom_bar(aes(fill = tbl_df[, 1]), 
+             stat = "identity", 
+             col = "white", 
+             size = 1, 
+             position = position_stack(reverse = TRUE)) +
+    geom_text(aes(x = center, 
+                  y = 1.05), 
+              label = tbl_p_df[, 2]) +
+    geom_text(aes(x = center, 
+                  y = label_height), 
+              label = format(ifelse(tbl_df[, 3] == 0, "", tbl_df[, 3]), 
+                             big.mark = ","), 
+              position = position_identity()) +
+    scale_x_continuous(breaks = x_breaks, 
+                       label = x_label) + 
+    scale_y_continuous(breaks = y_breaks,
+                       label = y_label) + 
+    theme(plot.margin = unit(c(1, 2, 1, 1), "lines"))
 }
